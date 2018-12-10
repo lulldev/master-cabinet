b19bccc9a49187cdb2df899cd74e77c10b356c86
lulldev <lulldev@yandex.ru>
2018-12-10 23:54:36 +0300

    create order ready UI

--- src/pages/ClientOrder.jsx
+++ src/pages/ClientOrder.jsx
@@ -11,6 +11,8 @@ import {
   ListItem,
   AccordionContent,
   Stepper,
+  BlockTitle,
+  Button,
 } from 'framework7-react';
 
 export default class Clients extends React.Component {
@@ -23,6 +25,7 @@ export default class Clients extends React.Component {
           </NavLeft>
           <NavTitle>Прием клиента</NavTitle>
         </Navbar>
+        <BlockTitle>Клиент</BlockTitle>
         <List>
           <ListItem title="Иванов Иван Иванович">
             <Icon slot="media" f7="person"></Icon>
@@ -34,27 +37,95 @@ export default class Clients extends React.Component {
             <Icon slot="media" f7="document_text"></Icon>
           </ListItem>
         </List>
+        <BlockTitle>Услуги</BlockTitle>
         <List accordionList>
           <ListItem accordionItem title="Услуга 1" smartSelect>
             <AccordionContent>
               <Block>
-              <List>
-                <ListItem header="Meeting starts in">
-                  <Stepper
-                    min={15}
-                    max={240}
-                    step={15}
-                    value={0}
-                    buttonsOnly={true}
-                    small
-                    slot="after"
-                   />
-                </ListItem>
+                <List>
+                  <ListItem header="02. Стрижка женская (средние волосы)'), 750 руб.">
+                    <Stepper
+                      min={0}
+                      max={10}
+                      step={1}
+                      wraps
+                      value={0}
+                      small
+                      slot="after"
+                    />
+                  </ListItem>
+                  <ListItem header="02. Стрижка женская (средние волосы)'), 750 руб.">
+                    <Stepper
+                      min={0}
+                      max={10}
+                      step={1}
+                      wraps
+                      value={0}
+                      small
+                      slot="after"
+                    />
+                  </ListItem>
                 </List>
               </Block>
             </AccordionContent>
           </ListItem>
         </List>
+        <BlockTitle>Дополнительно</BlockTitle>
+        <List accordionList>
+          <ListItem
+            title="Расходники"
+            smartSelect
+          >
+            <select name="fruits" multiple>
+              <option value="apple">Apple</option>
+              <option value="pineapple">Pineapple</option>
+              <option value="pear">Pear</option>
+            </select>
+          </ListItem>
+          <ListItem
+            title="Комплименты"
+            smartSelect
+          >
+            <select name="fruits" multiple>
+              <option value="apple">Apple</option>
+              <option value="pineapple">Pineapple</option>
+              <option value="pear">Pear</option>
+            </select>
+          </ListItem>
+          <ListItem accordionItem title="Рекомендуемые товары" smartSelect>
+            <AccordionContent>
+              <Block>
+                <List>
+                  <ListItem header="Товар 1, 750 руб.">
+                    <Stepper
+                      min={0}
+                      max={10}
+                      step={1}
+                      wraps
+                      value={0}
+                      small
+                      slot="after"
+                    />
+                  </ListItem>
+                  <ListItem header="Товар 2, 750 руб.">
+                    <Stepper
+                      min={0}
+                      max={10}
+                      step={1}
+                      wraps
+                      value={0}
+                      small
+                      slot="after"
+                    />
+                  </ListItem>
+                </List>
+              </Block>
+            </AccordionContent>
+          </ListItem>     
+        </List>
+        <Block>
+          <Button className="col" fill>Завершить</Button>
+        </Block>
       </Page>
     );
   }
