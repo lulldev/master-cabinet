44cf166c9b1b44e0ee393ca07f241cb82664b13d
lulldev <lulldev@yandex.ru>
2018-12-16 12:43:42 +0300

    add mock-api server

---
+++ node-mock-api/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const router = require('express').Router();
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+function mockFile(filename) {
+  return (request, response) => {
+    fs.createReadStream(filename).pipe(response);
+  };
+}
+
+app.get('/auth', mockFile('mocks/Auth.json'));
+
+app.listen(80, function () {
+  console.log('Mock web server listening on port 80');
+});
