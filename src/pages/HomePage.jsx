676c359563f72243cd44472690353a620a5e1948
lulldev <lulldev@yandex.ru>
2018-12-24 02:27:34 +0300

    pwa init

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,12 +9,15 @@ import {
     List,
     ListItem,
 } from 'framework7-react';
-import MasterProfile from '../components/MasterProfile';
+// import MasterProfile from '../components/MasterProfile';
 export default class HomePage extends React.Component {
   onLogout() {
-    window.location.href = '/index.php/api/auth_check';
+    window.location.href = '/index.php/main/logout';
   }
 
+  onBackToCabinet() {
+    window.location.href = '/index.php/main';
+  }
   render() {
     // const { onOpenLeftPanel } = this.props;
     return (
@@ -22,12 +25,13 @@ export default class HomePage extends React.Component {
         <Navbar>
           <NavTitle>Личный кабинет</NavTitle>
         </Navbar>
-        <MasterProfile />
+        {/* <MasterProfile /> */}
         <BlockTitle>Управление</BlockTitle>
         <List>
           <ListItem link="/clients/" title="Начать прием клиента" />
           <ListItem link="/proceeds/" title="Моя выручка за день" />
           <ListItem link="/notify-settings/" title="Настройка оповещений" />
+          <ListItem onClick={this.onBackToCabinet} title="Старая версия" />
           <ListItem onClick={this.onLogout} title="Выход" />
         </List>
       </Page>
